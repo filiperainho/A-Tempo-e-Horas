54735284970dc15e1c05baa073a8a9ec978e8325
rex.david.costa <rex.david.costa@gmail.com>
2013-12-08 20:24:44 +0000

    Limpar a class base de dados para o rainho inserir a dele

--- Servidor/src/pt/isec/gps/grupo12/servidor/Utilizadores/BaseDadosServidor.java
+++ Servidor/src/pt/isec/gps/grupo12/servidor/Utilizadores/BaseDadosServidor.java
@@ -4,57 +4,67 @@ import java.util.ArrayList;
 
 public class BaseDadosServidor implements FonteDadosServidor {
 	
-	class User{
-		String nome;
-		String userName;
-		String password;
-		User(String n, String nn, String p){
-			nome = n;
-			userName = nn;
-			password = p;
-		}
-	}
 	
-	ArrayList<User> lista;
-
-	public BaseDadosServidor(){
-		lista = new ArrayList<>();
-		lista.add(new User("Filipe Adduncao", "assuncao", "p"));
-		lista.add(new User("Filipe Rainho", "rainho", "p"));
-		lista.add(new User("David Goncalves", "david", "p"));
-		lista.add(new User("Erbi Silva", "erbi", "p"));
-		lista.add(new User("Samuel Cadima", "cadima", "p"));
-		System.out.println("BaseDados criada!");
-	}
-	
-	@Override
-	public String getPassword(String username) {
-		for(User s : lista){
-			if(s.userName.equals(username)){
-				return s.password;
-			}
-		}
-		return null;
-	}
-
-	@Override
-	public boolean userExiste(String username) {
-		for(User s : lista){
-			if(s.userName.equals(username)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	@Override
-	public String getNome(String username) {
-		for(User s : lista){
-			if(s.userName.equals(username)){
-				return s.nome;
-			}
-		}
-		return null;
-	}
 
 }
+
+
+
+
+
+
+
+// ANTIGO LIXO
+/*
+class User{
+	String nome;
+	String userName;
+	String password;
+	User(String n, String nn, String p){
+		nome = n;
+		userName = nn;
+		password = p;
+	}
+}
+
+ArrayList<User> lista;
+
+public BaseDadosServidor(){
+	lista = new ArrayList<>();
+	lista.add(new User("Filipe Adduncao", "assuncao", "p"));
+	lista.add(new User("Filipe Rainho", "rainho", "p"));
+	lista.add(new User("David Goncalves", "david", "p"));
+	lista.add(new User("Erbi Silva", "erbi", "p"));
+	lista.add(new User("Samuel Cadima", "cadima", "p"));
+	System.out.println("BaseDados criada!");
+}
+
+@Override
+public String getPassword(String username) {
+	for(User s : lista){
+		if(s.userName.equals(username)){
+			return s.password;
+		}
+	}
+	return null;
+}
+
+@Override
+public boolean userExiste(String username) {
+	for(User s : lista){
+		if(s.userName.equals(username)){
+			return true;
+		}
+	}
+	return false;
+}
+
+@Override
+public String getNome(String username) {
+	for(User s : lista){
+		if(s.userName.equals(username)){
+			return s.nome;
+		}
+	}
+	return null;
+}*/
